4a84986b6a9f6f025063a8e6ed1bc3669a5d5e95
DangJiaXu <196348363@qq.com>
2025-03-31 21:43:21 +0800

    实现了Knife4j接口文档,实现了用事件监听器读取Excel表格

--- src/main/java/user_center/once/ImportExcel.java
+++ src/main/java/user_center/once/ImportExcel.java
@@ -27,8 +27,7 @@ public class ImportExcel {
             log.error("类路径文件读取错误");
             throw new RuntimeException(e);
         }
-        // 这里默认每次会读取100条数据 然后返回过来 直接调用使用数据就行
-        // 具体需要返回多少行可以在`PageReadListener`的构造函数设置
+
         EasyExcel.read(resourceInputStream, TableUserInfo.class, new TableListener()).sheet().doRead();
 
     }
