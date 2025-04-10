fe9ba9f1223135928aadb4fb481fff3beecaea85
DangJiaXu <196348363@qq.com>
2025-04-10 18:42:35 +0800

    完成队伍和用户队伍关联表的创建以及增删改查

---
+++ src/main/java/user_center/service/TeamService.java
@@ -0,0 +1,13 @@
+package user_center.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import user_center.model.domain.Team;
+
+/**
+* @author 86139
+* @description 针对表【team(队伍)】的数据库操作Service
+* @createDate 2025-04-10 18:20:18
+*/
+public interface TeamService extends IService<Team> {
+
+}
---
+++ src/main/java/user_center/service/impl/UserTeamServiceImpl.java
@@ -0,0 +1,21 @@
+package user_center.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+import user_center.mapper.UserTeamMapper;
+import user_center.model.domain.UserTeam;
+import user_center.service.UserTeamService;
+
+/**
+* @author 86139
+* @description 针对表【user_team(用户队伍关系)】的数据库操作Service实现
+* @createDate 2025-04-10 18:24:03
+*/
+@Service
+public class UserTeamServiceImpl extends ServiceImpl<UserTeamMapper, UserTeam> implements UserTeamService {
+
+}
+
+
+
+
