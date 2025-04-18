f6fc1a9ebdd7b8669f9cb501a2b8296a3ca248c4
DangJiaXu <196348363@qq.com>
2025-04-18 20:17:14 +0800

    add 根据标签用最短路径算法匹配相似的用户

--- src/main/java/user_center/service/impl/UserServiceImpl.java
+++ src/main/java/user_center/service/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.math3.util.Pair;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.DigestUtils;
@@ -16,6 +17,7 @@ import user_center.exception.BusinessException;
 import user_center.mapper.UserMapper;
 import user_center.model.domain.User;
 import user_center.service.UserService;
+import user_center.utils.AlgorithmUtils;
 
 import java.util.*;
 import java.util.regex.Matcher;
@@ -26,13 +28,13 @@ import static user_center.constant.userConstant.ADMIN_ROLE;
 import static user_center.constant.userConstant.USER_LOGIN_STATE;
 
 /**
-* @author 86139
-* @description 针对表【user(用户)】的数据库操作Service实现
-* @createDate 2025-03-08 21:44:11
-*/
+ * @author 86139
+ * @description 针对表【user(用户)】的数据库操作Service实现
+ * @createDate 2025-03-08 21:44:11
+ */
 @Service
 @Slf4j
-public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService{
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
     @Resource
     private UserMapper userMapper;
@@ -43,39 +45,39 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     @Override
     public long userRegister(String userAccount, String userPassword, String checkPassword, String planetCode) {
         //校验用户输入是否合规
-        if(StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)){
+        if (StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "参数为空");
         }
-        if(userAccount.length() < 4){
+        if (userAccount.length() < 4) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户账户过短");
         }
-        if(userPassword.length() < 8 || checkPassword.length() < 8){
+        if (userPassword.length() < 8 || checkPassword.length() < 8) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户密码过短");
         }
-        if(planetCode.length() > 5){
+        if (planetCode.length() > 5) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "星球编号过长");
         }
         //账户不能包含特殊字符
         String validPattern = "[`~!@#$%^&*()+=|{}':;',\\[\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
         Matcher matcher = Pattern.compile(validPattern).matcher(userAccount);
-        if(matcher.find()){
+        if (matcher.find()) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
-        if(!userPassword.equals(checkPassword)){
+        if (!userPassword.equals(checkPassword)) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
         //账号不能重复
         QueryWrapper<User> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("userAccount", userAccount);
         long count = userMapper.selectCount(queryWrapper);
-        if(count > 0){
+        if (count > 0) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "注册账号重复");
         }
         //星球编号不能重复
         queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("planetCode", planetCode);
         count = userMapper.selectCount(queryWrapper);
-        if(count > 0){
+        if (count > 0) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "星球编号重复");
         }
         //加密密码
@@ -86,7 +88,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
         user.setUserPassword(encryptPassword);
         user.setPlanetCode(planetCode);
         boolean saveResult = this.save(user);
-        if(!saveResult){
+        if (!saveResult) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
         return user.getId();
@@ -95,19 +97,19 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     @Override
     public User userLogin(String userAccount, String userPassword, HttpServletRequest request) {
         //校验用户输入是否合规
-        if(StringUtils.isAnyBlank(userAccount, userPassword)){
+        if (StringUtils.isAnyBlank(userAccount, userPassword)) {
             throw new BusinessException(ErrorCode.NULL_ERROR);
         }
-        if(userAccount.length() < 4){
+        if (userAccount.length() < 4) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "账号过短");
         }
-        if(userPassword.length() < 8){
+        if (userPassword.length() < 8) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "密码过短");
         }
         //账户不能包含特殊字符
         String validPattern = "[`~!@#$%^&*()+=|{}':;',\\[\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
         Matcher matcher = Pattern.compile(validPattern).matcher(userAccount);
-        if(matcher.find()){
+        if (matcher.find()) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "账户不能包含特殊字符");
         }
         //加密匹配
@@ -115,17 +117,17 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
         //查询是否有此用户信息
         QueryWrapper<User> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("userAccount", userAccount);
-        queryWrapper.eq("userPassword",encryptPassword);
+        queryWrapper.eq("userPassword", encryptPassword);
         User user = userMapper.selectOne(queryWrapper);
         //用户不存在
-        if (user == null){
+        if (user == null) {
             log.info("user login failed,userAccount cannot match userPassword");
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户不存在");
         }
         //用户脱敏
         User safetyUser = getSafetyUser(user);
         //记录用户的登录态
-        request.getSession().setAttribute(USER_LOGIN_STATE,safetyUser);
+        request.getSession().setAttribute(USER_LOGIN_STATE, safetyUser);
 
         return safetyUser;
     }
@@ -137,8 +139,8 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     }
 
     @Override
-    public User getSafetyUser(User originUser){
-        if(originUser == null){
+    public User getSafetyUser(User originUser) {
+        if (originUser == null) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "脱敏对象为空");
         }
         User safetyUser = new User();
@@ -165,8 +167,8 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
      */
     //内存方式过滤标签
     @Override
-    public List<User> searchUsersByTags(List<String> tagNameList){
-        if(CollectionUtils.isEmpty(tagNameList)){
+    public List<User> searchUsersByTags(List<String> tagNameList) {
+        if (CollectionUtils.isEmpty(tagNameList)) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
         QueryWrapper<User> queryWrapper = new QueryWrapper<>();
@@ -174,10 +176,11 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
         Gson gson = new Gson();
         return userList.stream().filter(user -> {
             String tagsStr = user.getTags();
-            Set<String> tagSet = gson.fromJson(tagsStr, new TypeToken<Set<String>>(){}.getType());
+            Set<String> tagSet = gson.fromJson(tagsStr, new TypeToken<Set<String>>() {
+            }.getType());
             tagSet = Optional.ofNullable(tagSet).orElse(new HashSet<>());
             for (String tagNameInput : tagNameList) {
-                if(!tagSet.contains(tagNameInput)){
+                if (!tagSet.contains(tagNameInput)) {
                     return false;
                 }
             }
@@ -188,26 +191,26 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     @Override
     public int updateUser(User user, User loginUser) {
         Long inputUserLongId = user.getId();
-        if(inputUserLongId == null){
+        if (inputUserLongId == null) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
         //更新最近修改时间
         user.setUpdateTime(new Date());
         //更改密码时加密
-        if(user.getUserPassword() != null){
+        if (user.getUserPassword() != null) {
             user.setUserPassword(DigestUtils.md5DigestAsHex((SALT + user.getUserPassword()).getBytes()));
         }
         int inputUserId = inputUserLongId.intValue();
-        if(inputUserId <= 0){
+        if (inputUserId <= 0) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
         //如果是管理员，可以修改所有的用户
         //不是管理员只能修改自己的信息
-        if(!isAdmin(loginUser) && inputUserId != loginUser.getId()){
+        if (!isAdmin(loginUser) && inputUserId != loginUser.getId()) {
             throw new BusinessException(ErrorCode.NO_AUTH);
         }
         User oldUser = userMapper.selectById(inputUserId);
-        if(oldUser == null){
+        if (oldUser == null) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
         return userMapper.updateById(user);
@@ -215,11 +218,11 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 
     @Override
     public User getLoginUser(HttpServletRequest request) {
-        if(request == null){
+        if (request == null) {
             return null;
         }
         Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
-        if(userObj == null){
+        if (userObj == null) {
             throw new BusinessException(ErrorCode.NOT_LOGIN);
         }
         return (User) userObj;
@@ -227,6 +230,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 
     /**
      * 判断是否为管理员
+     *
      * @param request request
      * @return boolean
      */
@@ -239,6 +243,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 
     /**
      * 判断是否为管理员
+     *
      * @param loginUser 当前登录用户
      * @return boolean
      */
@@ -247,10 +252,60 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
         return loginUser != null && loginUser.getUserRole() == ADMIN_ROLE;
     }
 
+    @Override
+    public List<User> matchUsers(long num, User loginUser) {
+        String loginUserTags = loginUser.getTags();
+        if (StringUtils.isBlank(loginUserTags) || "[]".equals(loginUserTags)) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "当前登录用户标签为空");
+        }
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select("id", "tags");
+        queryWrapper.isNotNull("tags");
+        List<User> userList = this.list(queryWrapper);
+        Gson gson = new Gson();
+        List<String> loginUserTagList = gson.fromJson(loginUserTags, new TypeToken<List<String>>() {
+        }.getType());
+        //用户 => 相似度
+        List<Pair<User, Long>> list = new ArrayList<>();
+//        SortedMap<Integer, Long> indexDistanceMap = new TreeMap<>();
+        //依次计算所有用户和当前用户的相似度
+        for (User user : userList) {
+            String userTags = user.getTags();
+            //无标签或为用户自己
+            if (StringUtils.isBlank(userTags) || Objects.equals(user.getId(), loginUser.getId())) {
+                continue;
+            }
+            List<String> userTagList = gson.fromJson(userTags, new TypeToken<List<String>>() {
+            }.getType());
+            //计算分数
+            long distance = AlgorithmUtils.minDistance(loginUserTagList, userTagList);
+            list.add(new Pair<>(user, distance));
+        }
+        List<Pair<User, Long>> topUserPairList = list.stream()
+                .sorted((a, b) -> (int) (a.getValue() - b.getValue()))
+                .limit(num)
+                .toList();
+        List<Long> topUserIdList = topUserPairList.stream()
+                .map(pair -> pair.getKey().getId())
+                .toList();
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+        userQueryWrapper.in("id", topUserIdList);
+        //用list得到的是无序的
+        Map<Long, List<User>> userIdUserListMap = this.list(userQueryWrapper).stream()
+                .map(this::getSafetyUser)
+                .collect(Collectors.groupingBy(User::getId));
+        //排序
+        List<User> finalUserList = new ArrayList<>();
+        for (Long userId : topUserIdList) {
+            finalUserList.add(userIdUserListMap.get(userId).get(0));
+        }
+        return finalUserList;
+    }
+
     //SQL方式过滤标签
     @Deprecated
-    private List<User> searchUsersByTagsBySQL(List<String> tagNameList){
-        if(CollectionUtils.isEmpty(tagNameList)){
+    private List<User> searchUsersByTagsBySQL(List<String> tagNameList) {
+        if (CollectionUtils.isEmpty(tagNameList)) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
         QueryWrapper<User> queryWrapper = new QueryWrapper<>();
